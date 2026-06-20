bff9ead603206d2515a9f9d4b7e0a9379e8dcce1
Gustavo Mota <motagustavo039@gmail.com>
2026-06-20 14:07:58 -0300

    imprimir as rotações

--- src/TesteDesempenho.java
+++ src/TesteDesempenho.java
@@ -12,75 +12,52 @@ public class TesteDesempenho {
         Scanner sc = new Scanner(System.in);
 
         while(true){
-
             System.out.println("\n===== MENU =====");
             System.out.println("1 - Testar com 100 registros");
             System.out.println("2 - Testar com 1000 registros");
             System.out.println("3 - Testar com 5000 registros");
             System.out.println("0 - Sair");
-
             int opcao = sc.nextInt();
-
             if(opcao == 0)
                 break;
-
             int limite = 0;
-
             switch(opcao){
                 case 1:
                     limite = 100;
                     break;
-
                 case 2:
                     limite = 1000;
                     break;
-
                 case 3:
                     limite = 5000;
                     break;
-
                 default:
                     System.out.println("Opção inválida");
                     continue;
             }
-
             System.out.println("\nDeseja exibir os registros?");
             System.out.println("1 - Sim");
             System.out.println("2 - Não");
-
             boolean exibir = sc.nextInt() == 1;
-
             executarTeste(limite, exibir);
         }
-
         sc.close();
     }
-
     public static void executarTeste(int limite, boolean exibir){
-
         try{
-
             String arquivo = "src/complete_fragrantica_dataset.csv";
-
             Arvore_BST bst = new Arvore_BST();
-
             BufferedReader br =
                     new BufferedReader(
                             new FileReader(arquivo)
                     );
-
             br.readLine();
-
             String linha;
-
             int contador = 0;
-
             long inicioBST = System.nanoTime();
-
             while((linha = br.readLine()) != null && contador < limite){
 
                 String[] dados = linha.split(";");
-
                 int id = Integer.parseInt(dados[0]);
                 String nome = dados[1];
                 String brand = dados[2];
@@ -97,7 +74,6 @@ public class TesteDesempenho {
                 bst.add(
                         new Tinfo(id,nome,brand,country,sexo,ratingVAL,ratingCountry,ano,top,midlle,base,perfurmer,mainaccon)
                 );
-
                 if(exibir){
                     System.out.println(
                             id + " - " + nome+brand+sexo+ratingVAL+ratingCountry+ano+top+midlle+base+perfurmer+mainaccon
@@ -157,14 +133,11 @@ public class TesteDesempenho {
             System.out.println("\nBST");
 
             System.out.println(
-                    "Tempo: "
-                            + (fimBST - inicioBST)
-                            + " ns"
+                    "Tempo: " + (fimBST - inicioBST) + " ns"
             );
 
             System.out.println(
-                    "Altura: "
-                            + bst.altura(bst.raiz())
+                    "Altura: " + bst.altura(bst.raiz())
             );
 
             System.out.println(
@@ -175,24 +148,19 @@ public class TesteDesempenho {
             System.out.println("\nAVL");
 
             System.out.println(
-                    "Tempo: "
-                            + (fimAVL - inicioAVL)
-                            + " ns"
+                    "Tempo: " + (fimAVL - inicioAVL) + " ns"
             );
 
             System.out.println(
-                    "Altura: "
-                            + avl.altura(avl.raiz())
+                    "Altura: " + avl.altura(avl.raiz())
             );
 
             System.out.println(
-                    "Comparações: "
-                            + avl.getComparacoes()
+                    "Comparações: " + avl.getComparacoes()
             );
 
             System.out.println(
-                    "Rotações: "
-                            + avl.getRotacoes()
+                    "Rotações: " + avl.getRotacoes()
             );
 
         }
